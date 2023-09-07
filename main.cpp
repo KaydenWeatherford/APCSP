1c4c1016ecc3425632b2e438d8d18ae856d4a743
KaydenWeatherford <143819214+KaydenWeatherford@users.noreply.github.com>
2023-09-07 14:26:46 +0000

    Sum & Average

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello World!";
+int main()
+{
+   int A,B,C,D,sum;
+    float average;
+
+    cout << "Enter 4 Numbers : " << endl;
+    cin >> A >> B >> C >> D;
+    sum = A + B + C + D;
+    average=sum/4;
+    cout << "The sum of " << A << " , " << B << " , " << C << " , " << D << " is " << sum << "." << endl;
+    cout << "The average of the numbers is " << average << "." << endl;
     return 0;
 }
