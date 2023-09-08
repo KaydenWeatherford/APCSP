24d8e235b0734aa2155804000a0b0cd2e06b086f
KaydenWeatherford <143819214+KaydenWeatherford@users.noreply.github.com>
2023-09-08 14:25:27 +0000

    Sum & Average Update

---
+++ Temp.md
@@ -0,0 +1,4 @@
+# Just some Temp Notes
+
+Run:
+g++ -o main.exe main.cpp && ./main.exe
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-   int A,B,C,D,sum;
+   float A,B,C,D,sum;
     float average;
 
     cout << "Enter 4 Numbers : " << endl;
@@ -11,6 +11,7 @@ int main()
     sum = A + B + C + D;
     average=sum/4;
     cout << "The sum of " << A << " , " << B << " , " << C << " , " << D << " is " << sum << "." << endl;
-    cout << "The average of the numbers is " << average << "." << endl;
+    cout << "The average of the numbers is " << average << endl;
     return 0;
+  
 }
