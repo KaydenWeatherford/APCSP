95d7431142596743a1d2aa1b5a237bc8989b4fa0
KaydenWeatherford <143819214+KaydenWeatherford@users.noreply.github.com>
2023-09-26 13:57:58 +0000

    115b

---
+++ Assignments/115b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstdio>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+int num = 2;
+  while (num <= 36)  
+    {
+    cout << num << " ";
+    num = num + 2;
+    }
+ return 0; 
+}
--- MDs/Temp.md
+++ MDs/Temp.md
@@ -1,4 +1,4 @@
 # Just some Temp Notes
 
 Run:
-g++ -o Assignments/115a.exe Assignments/115a.cpp && ./Assignments/115a.exe
+g++ -o Assignments/115b.exe Assignments/115b.cpp && ./Assignments/115b.exe
