443dea621a5d6cd2749ac3171e12b1e3c61ed2ab
KaydenWeatherford <143819214+KaydenWeatherford@users.noreply.github.com>
2024-04-01 14:01:39 +0000

    MSOE 2015 4

--- Assignments/MSOE_2015_4.cpp
+++ Assignments/MSOE_2015_4.cpp
@@ -4,28 +4,54 @@
 #include <array>
 #include <cmath>
 #include <vector>
-#include <algoritm>
+#include <bits/stdc++.h> 
 using namespace std;
 
-// g++ -o "MSOE_2015_4.exe" "Assignements/MSOE_2015_4.cpp" && ./"MSOE_2015_4.exe"
+// g++ -o "MSOE_2015_4.exe" "Assignments/MSOE_2015_4.cpp" && ./"MSOE_2015_4.exe"
 int main()
 {
-  vector<double> numbers;
-  double x;
+  vector<int> numbers;
+  double Third;
+  double x, y;
   int R;
-
-  while (R != 0)
+  int round = 0;
+  
+  while (R != 10)
   {
     cout << "Enter a number: \n";
     cin >> x;
 
+    if(!cin)
+    {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // https://stackoverflow.com/questions/5655142
     
+    }
+    else
+    {
+    round = floor(100 * x + 0.5f);
+    numbers.push_back(round);
+    sort(numbers.begin(), numbers.end());
+    
+    cout << "Sorted \n"; 
+    for (double x : numbers)
+    {
+      cout << (x/100) << " "; 
+    }
+    cout << "\n";
 
 
-    
+    if (R >= 3)
+    {
+      y = numbers[R-2];
+      Third = y / 100;
+      cout << Third << endl;
+    }
+    R++;
+      
+    }
   }
-  sort( numbers.begin(), numbers.end() );
-  
+
   for (vector<int>::const_iterator it=numbers.begin(); it!=numbers.end(); ++it) {
     cout << *it << " ";
   }
